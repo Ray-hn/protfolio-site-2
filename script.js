8ec0c7fbe3ac0e1320e2b5df47398012064ae26d
rayhan <rayhanali2029@gmail.com>
2023-03-23 09:26:12 -0700

    protfolio-2

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+// Typing Script JS
+var typed = new Typed(".typing",{
+    strings: ["Web Designer", "Web Developer", "Youtuber", "Blogger"],
+    typeSpeed: 80,
+    backSpeed: 80,
+});
+
+var typed = new Typed(".typing-2",{
+    strings: ["Web Developer", "Web Designer", "Youtuber", "Blogger"],
+    typeSpeed: 100,
+    backSpeed: 60,
+});
+
+
